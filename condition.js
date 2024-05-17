1cfcc953845383bb4a305dff869f034327214d6e
kazipintu <kazipintu@gmail.com>
2024-05-17 23:21:37 +0530

    MERN/Devzone/JS/condition

--- condition.js
+++ condition.js
@@ -2,70 +2,145 @@
 
 // if-else
 
-var arbitraryNumber1 = 15;
-if (arbitraryNumber1 % 2 == 0) {
-  console.log('15 is even number');
-} else {
-  console.log('15 is odd number');
-}
+{
+  // if data true, then it will return from body, if false it will directly go to else statement //
+  data;
+  if (data) {
 
-var arbitraryNumber2 = 26;
-if (arbitraryNumber2 % 2 == 0) {
-  console.log('26 is even number');
-} else {
-  console.log('26 is odd number');
-}
+  } else {
 
-var ageGroom = 21;
-if (ageGroom > 20) {
-  console.log('Groom is eligible for marriage');
-} else {
-  console.log('Groom is not eligible for marriage');
+  }
 }
 
 
-var ageBride = 15;
-if (ageBride > 18) {
-  console.log('Bride is eligible for marriage');
-} else {
-  console.log('Bride is not eligible for marriage');
-}
-
-var ageMan = 21;
-var jobHolderMan = true;
-var salaryMan = 5000;
-var handsomeMan = true;
-var ownHomeMan = false;
-var singleMan = true;
-
-
-if (jobHolderMan && salaryMan > 4500 && handsomeMan && ownHomeMan && singleMan) {
-  console.log('Man can be eligible groom');
-} else {
-  console.log('Man is not eligible groom');
-}
-
-if (jobHolderMan && salaryMan > 4500 && handsomeMan && singleMan) {
-  console.log('Man can be eligible groom');
-} else {
-  console.log('Man is not eligible groom');
+{
+  console.log(5 > 6);
+  console.log(5 < 6);
+  console.log(5 == 6);
+  console.log(5 != 6);
+  console.log(5 >= 6);
+  console.log(5 <= 6);
 }
 
 
-var tourPlan = ['dubai', 'nepal', 'dhaka', 'digha', 'mirik'];
+{
+  const iPhone = 1000;
+  const fund = 999;
 
-if (tourPlan[1] == 'dubai') {
-  console.log('Dubai tour is planned');
-} else if (tourPlan[2] == 'mirik') {
-  console.log('plan for Mirik');
-} else {
-  console.log('plan for Digha');
+  if (fund > iPhone) {
+    console.log('You can purchase');
+  }
+  else {
+    console.log('Lack of fund');
+  };
+
+  if (fund > iPhone == true) {
+    console.log('You can purchase');
+  }
+  else {
+    console.log('Lack of fund');
+  };
 }
 
-if (tourPlan.indexOf('doha') == -1) {
-  console.log('we will tour to Qatar');
-} else {
-  console.log("we can't go anywhere");
+
+{
+  const iPhone = 1000;
+  const fund = 1999;
+
+  if (fund > iPhone == true) {
+    console.log('You can purchase');
+  }
+  else {
+    console.log('Lack of fund');
+  };
+}
+
+
+{
+  const jobHolder = true;
+  const savedMoney = 5000000;
+  const ownHomeKolkata = false;
+  const homeDistrict = false;
+
+  if (jobHolder == true && savedMoney >= 400000 || ownHomeKolkata == true && homeDistrict == true) {
+    console.log("Marriage is allowed");
+  }
+  else {
+    console.log("Proposal is rejected");
+  }
+}
+
+{
+  var arbitraryNumber1 = 15;
+  if (arbitraryNumber1 % 2 == 0) {
+    console.log('15 is even number');
+  } else {
+    console.log('15 is odd number');
+  }
+
+  var arbitraryNumber2 = 26;
+  if (arbitraryNumber2 % 2 == 0) {
+    console.log('26 is even number');
+  } else {
+    console.log('26 is odd number');
+  }
+
+  var ageGroom = 21;
+  if (ageGroom > 20) {
+    console.log('Groom is eligible for marriage');
+  } else {
+    console.log('Groom is not eligible for marriage');
+  }
+
+
+  var ageBride = 15;
+  if (ageBride > 18) {
+    console.log('Bride is eligible for marriage');
+  } else {
+    console.log('Bride is not eligible for marriage');
+  }
+}
+
+
+{
+  var ageMan = 21;
+  var jobHolderMan = true;
+  var salaryMan = 5000;
+  var handsomeMan = true;
+  var ownHomeMan = false;
+  var singleMan = true;
+
+
+  if (jobHolderMan && salaryMan > 4500 && handsomeMan && ownHomeMan && singleMan) {
+    console.log('Man can be eligible groom');
+  } else {
+    console.log('Man is not eligible groom');
+  }
+
+  if (jobHolderMan && salaryMan > 4500 && handsomeMan && singleMan) {
+    console.log('Man can be eligible groom');
+  } else {
+    console.log('Man is not eligible groom');
+  }
+}
+
+
+{
+  var tourPlan = ['dubai', 'nepal', 'dhaka', 'digha', 'mirik'];
+
+  if (tourPlan[1] == 'dubai') {
+    console.log('Dubai tour is planned');
+  } else if (tourPlan[2] == 'mirik') {
+    console.log('plan for Mirik');
+  } else {
+    console.log('plan for Digha');
+  }
+
+  if (tourPlan.indexOf('doha') == -1) {
+    console.log('we will tour to Qatar');
+  } else {
+    console.log("we can't go anywhere");
+  }
 }
 
 
@@ -127,6 +202,25 @@ switch (action) {
     break;
   default:
     console.log("empty action received");
+}
+
+
+{
+  const data = 4;
+
+  switch (data) {
+    case 1:
+      console.log('it is 2');
+      break;
+    case 2:
+      console.log('it is 3');
+      break;
+    case 3:
+      console.log("it is also false");
+      break;
+
+  }
+
 }
 
 /*  ~ take input in JavaScript as prompt ~  */
